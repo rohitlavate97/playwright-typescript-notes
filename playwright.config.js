36479d0f0f7ec3cf7401b97974bbaecac6d99b0f
Rohit Lavate <rohitlavate97@gmail.com>
2026-02-28 12:18:42 +0530

    Modified playwright.config.js file

--- playwright.config.js
+++ playwright.config.js
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
-export default defineConfig({
+const config = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -28,7 +28,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
+    browserName: 'chromium',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -78,4 +78,4 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
+module.exports = config;
