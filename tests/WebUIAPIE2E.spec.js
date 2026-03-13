ebfed157c915dc9e57854c189af9a78b1a3242c5
Rohit Lavate <rohitlavate97@gmail.com>
2026-03-13 10:35:26 +0530

    Added Place the order test

--- tests/WebUIAPIE2E.spec.js
+++ tests/WebUIAPIE2E.spec.js
@@ -28,7 +28,7 @@ test.beforeAll("Extracting token", async() =>{
     console.log(token);
 });
 
-test.only("E2E testing with use of UI and API's", async({browser}) =>{
+test("E2E testing with use of UI and API's", async({browser}) =>{
     const context = await browser.newContext();
     const page = await context.newPage();
     page.addInitScript(value => {
@@ -51,6 +51,38 @@ test.only("E2E testing with use of UI and API's", async({browser}) =>{
     console.log(orderId);
 });
 
-// test.only("API Test", async()=>{
-
-// });
+test.only("Place the order", async({browser})=>{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    page.addInitScript(value => {
+        window.localStorage.setItem('token',value);
+    }, token);
+    const productName = 'ZARA COAT 3';
+    await page.goto("https://rahulshettyacademy.com/client/auth/user");
+    const products = page.locator('.card-body');
+    await page.locator('.card-body b').first().waitFor();
+    await products.filter({hasText: productName}).getByRole("button",{name:" Add To Cart"}).click();
+    const orderResponse =await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",{
+        data: orderPayload,
+        headers: {
+           'Authorization': token,
+           'Content-Type': 'application/json'
+        }
+    })
+    const orderResponseJson = await orderResponse.json();
+    orderId = orderResponseJson.orders[0];
+    console.log(orderId);
+    await page.goto("https://rahulshettyacademy.com/client/");
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.locator("tbody").waitFor();
+    const rows = await page.locator("tbody tr");
+    for(let i=0;i<await rows.count();i++){
+        const rowOrderId = await rows.nth(i).locator("th").textContent();
+        if(orderId.includes(rowOrderId)){
+            await rows.nth(i).locator("button").first().click();
+            break;
+        }
+    }
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+});
